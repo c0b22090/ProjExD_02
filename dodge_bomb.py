3b9c632356c30f2307e79a4decb2b8043ce20056
Takiguchi <c0b22090b2@edu.teu.ac.jp>
2023-06-20 14:12:35 +0900

    練習１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import pygame as pg
 
@@ -10,9 +11,14 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
+    enn = pg.Surface((20,20))
+    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
+    enn.set_colorkey((0, 0, 0))
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     clock = pg.time.Clock()
     tmr = 0
+    x = random.randint(0, WIDTH)
+    y = random.randint(0, HEIGHT)
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -20,6 +26,9 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        enn_rct = enn.get_rect()
+        enn_rct.center = x, y
+        screen.blit(enn, enn_rct)
         pg.display.update()
         tmr += 1
         clock.tick(10)
