6cafb3d6eb2180638deeb1d2bddce3fb9d6d97dd
Takiguchi <c0b22090b2@edu.teu.ac.jp>
2023-06-20 14:53:49 +0900

    練習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,6 +4,7 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1600, 900
+enn_lst = {pg.K_UP: (0, -5), pg.K_DOWN: (0, +5), pg.K_LEFT: (-5, 0), pg.K_RIGHT: (+5, 0)}
 
 
 def main():
@@ -15,23 +16,36 @@ def main():
     pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
     enn.set_colorkey((0, 0, 0))
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
     clock = pg.time.Clock()
     tmr = 0
     x = random.randint(0, WIDTH)
     y = random.randint(0, HEIGHT)
+    enn_rct = enn.get_rect()
+    enn_rct.center = x, y
+    vx, vy = +5, +5
+
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0,0]
+        for k, mv in enn_lst.items():
+            if key_lst[k]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv)
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
-        enn_rct = enn.get_rect()
-        enn_rct.center = x, y
+        screen.blit(kk_img, kk_rct)
+        enn_rct.move_ip(vx, vy)
         screen.blit(enn, enn_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
