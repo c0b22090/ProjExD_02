77e6306175811497d958e08a938056a4a4d2a553
Takiguchi <c0b22090b2@edu.teu.ac.jp>
2023-06-20 15:29:01 +0900

    練習４、５

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -7,6 +7,15 @@ WIDTH, HEIGHT = 1600, 900
 enn_lst = {pg.K_UP: (0, -5), pg.K_DOWN: (0, +5), pg.K_LEFT: (-5, 0), pg.K_RIGHT: (+5, 0)}
 
 
+def check_bound(rect: pg.Rect) -> tuple[bool, bool]:
+    yoko, tate = True, True
+    if rect.left < 0 or WIDTH < rect.right:
+        yoko = False
+    if rect.top < 0 or HEIGHT < rect.bottom:
+        tate = False
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -31,6 +40,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
+        if kk_rct.colliderect(enn_rct):
+            print("ゲームオーバー")
+            return
+        
         key_lst = pg.key.get_pressed()
         sum_mv = [0,0]
         for k, mv in enn_lst.items():
@@ -38,10 +52,17 @@ def main():
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
         enn_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(enn_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(enn, enn_rct)
         pg.display.update()
         tmr += 1
