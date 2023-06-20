d28f31858f9c667f0ff0ee37b0e60860869c32a0
Takiguchi <c0b22090b2@edu.teu.ac.jp>
2023-06-20 15:56:26 +0900

    演習１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -21,10 +21,19 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
+    kk_img_f = pg.transform.flip(kk_img, True, False)
     enn = pg.Surface((20,20))
     pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
     enn.set_colorkey((0, 0, 0))
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img_1 = pg.transform.rotozoom(kk_img, 315, 1.0)
+    kk_img_2 = pg.transform.rotozoom(kk_img, 0, 1.0)
+    kk_img_3 = pg.transform.rotozoom(kk_img, 45, 1.0)
+    kk_img_4 = pg.transform.rotozoom(kk_img_f, 270, 2.0)
+    kk_img_5 = pg.transform.rotozoom(kk_img_f, 315, 2.0)
+    kk_img_6 = pg.transform.rotozoom(kk_img_f, 0, 2.0)
+    kk_img_7 = pg.transform.rotozoom(kk_img_f, 45, 2.0)
+    kk_img_8 = pg.transform.rotozoom(kk_img_f, 90, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
     clock = pg.time.Clock()
@@ -34,6 +43,8 @@ def main():
     enn_rct = enn.get_rect()
     enn_rct.center = x, y
     vx, vy = +5, +5
+    kk_img_lst = [kk_img_1, kk_img_2, kk_img_3, kk_img_4, kk_img_5, kk_img_6, kk_img_7, kk_img_8]
+    kk_mv_xy = [[-5, -5], [-5, 0], [-5, +5], [0, +5], [+5, +5], [+5, 0], [+5, -5], [0, -5]]
 
 
     while True:
@@ -54,6 +65,10 @@ def main():
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+
+        for i in range(len(kk_img_lst)):
+            if kk_mv_xy[i] == sum_mv:
+                kk_img = kk_img_lst[i]
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
